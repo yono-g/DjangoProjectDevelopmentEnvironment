00fa7c5f4bf7ed8d493c57a20ed46d0a389fd138
yono <firebrick26@gmail.com>
2019-07-11 18:49:43 +0900

    Configure database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'db',
+        'NAME': 'mysite',
+        'USER': 'dev',
+        'PASSWORD': 'dev',
     }
 }
 
