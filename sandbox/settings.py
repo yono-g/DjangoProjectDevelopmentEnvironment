1434f30bce55da766a65e1aaa287269d86351831
yono <firebrick26@gmail.com>
2019-07-19 13:00:15 +0900

    Move accounts app to under mysite package

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sandbox.accounts.apps.AccountsConfig',
     'sandbox.model_performance.apps.ModelPerformanceConfig',
     'sandbox.customcommand.apps.CustomcommandConfig',
 ]
