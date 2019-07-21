1b9b1b44b7790c4381cf92bcaceb8926b859bcad
yono <firebrick26@gmail.com>
2019-07-21 11:11:11 +0900

    Implement email authentication

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from .views import IndexView
+from .forms import EmailAuthenticationForm
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='accounts.login'),
+    path('login/', auth_views.LoginView.as_view(
+            template_name='accounts/login.html',
+            form_class=EmailAuthenticationForm
+        ), name='accounts.login'),
     path('', IndexView.as_view(), name='accounts.index'),
 ]
