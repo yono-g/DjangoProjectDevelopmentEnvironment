8688f5207a2efd7aceef27d2d655d1fb48e597d8
yono <firebrick26@gmail.com>
2019-07-22 17:55:24 +0900

    Add logout view

--- mysite/accounts/tests.py
+++ mysite/accounts/tests.py
@@ -14,3 +14,7 @@ class LoginTest(TestCase):
     def test_login(self):
         response = self.client.post('/login/', self.credentials, follow=True)
         self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_logout(self):
+        response = self.client.get('/logout/', follow=True)
+        self.assertFalse(response.context['user'].is_authenticated)
