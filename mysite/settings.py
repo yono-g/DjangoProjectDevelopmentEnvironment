5564fbea9c879e4f9feb64c9c09a404531982269
yono <firebrick26@gmail.com>
2019-07-22 20:07:13 +0900

    Add password reset feature

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,3 +130,8 @@ AUTH_USER_MODEL = 'accounts.User'
 LOGIN_URL = 'accounts.login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'accounts.login'
+
+
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
