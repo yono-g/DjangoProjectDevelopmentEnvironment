77ae29c9d0a4f3adf26193a28cd2b402ae72c08e
yono <firebrick26@gmail.com>
2019-08-03 14:12:37 +0900

    Add full_name to User model

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,3 +138,10 @@ LOGOUT_REDIRECT_URL = 'accounts.login'
 # Email settings
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# i18n settings
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'accounts')
+]
